ce22af1397077ebbcb810da76ec45ce29d0cd606
Geefirce <gjonah554@gmail.com>
2025-05-22 16:09:03 +0100

    initial commit with multiple React projects

---
+++ 04-steps/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+
+// REACT V18
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
---
+++ date-counter/src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+
+export default function App() {
+  return <Counter />;
+}
+
+function Counter() {
+  return <div>TEST</div>;
+}
